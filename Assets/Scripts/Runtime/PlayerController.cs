76a6ab689068630749f2739dd73036df223abe7b
Yamamoto,Taku <jts0ymt@gmail.com>
2021-01-24 11:05:42 +0900

    プレイヤー移動追加

--- Assets/Scripts/Runtime/PlayerController.cs
+++ Assets/Scripts/Runtime/PlayerController.cs
@@ -1,29 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Runtime
 {
-    public class PlayerController : MonoBehaviour
-    {
-        public PlayerInformation playerInformation;
-        public Transform target;
+	public class PlayerController : MonoBehaviour
+	{
+		public PlayerInformation playerInformation;
+		public Transform target;
 		public Transform weaponPlace;
-        private CharacterController mController { get; set; }
+		private CharacterController mController { get; set; }
 
+		private Vector3 move;
 
-		void Awake(){
-			
+		public void OnMove(InputAction.CallbackContext context)
+		{
+			move = context.ReadValue<Vector2>();
 		}
 
-        void Start()
-        {
-            playerInformation.transform = target.transform;
-            mController = GetComponent<CharacterController>();
-        }
+		void Start()
+		{
+			playerInformation.transform = target.transform;
+			mController = GetComponent<CharacterController>();
+		}
 
 
-        void Update()
-        {
-            
-        }
-    }
+		void Update()
+		{
+			mController.Move(move);
+		}
+
+
+
+	}
 }
